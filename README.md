4bfb8ca5df34b6b41fff33ae235a88369e37f6f2
Julien Béranger <julien@strat.cc>
2023-10-27 16:01:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Tuesday Heartbreak
+
+A variant of [Nexth](https://github.com/wslyvh/nexth/), a boilerplate built by [wslyvh](https://github.com/wslyvh) and [others](https://github.com/wslyvh/nexth/graphs/contributors).
+
+## Features
+
+- [Next.js](https://nextjs.org/docs)
+- [Chakra UI](https://chakra-ui.com/)
+- [Ethers v6](https://docs.ethers.org/v6/)
+- [viem](https://viem.sh/)
+- [wagmi](https://wagmi.sh/)
+- [Web3Modal SDK from WalletConnect](https://docs.walletconnect.com/)
+- [Sign-In with Ethereum](https://www.login.xyz/)
+- [usehooks-ts](https://usehooks-ts.com/)
+- [next-SEO](https://github.com/garmeeh/next-seo)
+- [TypeScript](https://www.typescriptlang.org/)
+- [eslint](https://eslint.org/)
+- [prettier](https://prettier.io/)
+- [Husky](https://typicode.github.io/husky/)
+
+## Install
+
+```bash
+pnpm install
+```
+
+## Run
+
+Create a `.env` on the model of `.env.template`:
+
+```sh
+cp .env.template .env
+```
+
+Add your own keys in your `.env` file, then:
+
+```bash
+pnpm dev
+```
+
+## Build
+
+```bash
+pnpm build
+```
+
+## Versions
+
+- Node [v18.17.1](https://nodejs.org/uk/blog/release/v18.17.1/)
+- PNPM [v8.7.5](https://pnpm.io/pnpm-vs-npm)
+
+## Support
+
+You can contact me via [Element](https://matrix.to/#/@julienbrg:matrix.org), [Telegram](https://t.me/julienbrg), [Twitter](https://twitter.com/julienbrg), [Discord](https://discord.gg/uSxzJp3J76), or [LinkedIn](https://www.linkedin.com/in/julienberanger/).
---
+++ src/utils/config.ts
@@ -0,0 +1,44 @@
+import { ThemingProps } from '@chakra-ui/react'
+import { Chain } from '@wagmi/core'
+
+export const SITE_NAME = 'Tuesday Heartbreak'
+export const SITE_DESCRIPTION = 'I want to be with you when the night time comes'
+export const SITE_URL = 'tuesday-heartbreak.netlify.app'
+
+export const THEME_INITIAL_COLOR = 'system'
+export const THEME_COLOR_SCHEME: ThemingProps['colorScheme'] = 'gray'
+export const THEME_CONFIG = {
+  initialColorMode: THEME_INITIAL_COLOR,
+}
+
+export const SOCIAL_TWITTER = 'W3HC'
+export const SOCIAL_GITHUB = 'w3hc/tuesday-heartbreak'
+
+export const artheraTestnet: Chain = {
+  id: 10243,
+  name: 'Arthera Testnet',
+  network: 'artheraTestnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'AA',
+    symbol: 'AA',
+  },
+  rpcUrls: {
+    public: { http: ['https://rpc-test.arthera.net'] },
+    default: { http: ['https://rpc-test.arthera.net'] },
+  },
+  blockExplorers: {
+    etherscan: { name: 'Arthera Testnet Explorer', url: 'https://explorer-test.arthera.net' },
+    default: { name: 'Arthera Testnet Explorer', url: 'https://explorer-test.arthera.net' },
+  },
+} as const satisfies Chain
+
+export const ETH_CHAINS = [artheraTestnet]
+
+export const SERVER_SESSION_SETTINGS = {
+  cookieName: SITE_NAME,
+  password: process.env.SESSION_PASSWORD ?? 'UPDATE_TO_complex_password_at_least_32_characters_long',
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production',
+  },
+}
