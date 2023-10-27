c04b4e11a2d335d8a814e7f6b1fd1c6752178dca
Julien Béranger <julien@strat.cc>
2023-10-27 16:17:43 +0200

    Add iframe (#2)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -86,8 +86,16 @@ export default function Home() {
       <Head />
 
       <main>
-        <HeadingComponent as="h2">Hi there! 👋</HeadingComponent>
-        <Button
+        <HeadingComponent as="h2">Thirdweb iFrame test</HeadingComponent>
+
+        <iframe
+          src="https://embed.ipfscdn.io/ipfs/bafybeicd3qfzelz4su7ng6n523virdsgobrc5pcbarhwqv3dj3drh645pi/?contract=0x55153a49412a94aDc4Cee3AcFEBa64e2BB574C6c&chain=%7B%22name%22%3A%22Goerli%22%2C%22chain%22%3A%22ETH%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fgoerli.rpc.thirdweb.com%2F%24%7BTHIRDWEB_API_KEY%7D%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Goerli+Ether%22%2C%22symbol%22%3A%22ETH%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22gor%22%2C%22chainId%22%3A5%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22goerli%22%2C%22icon%22%3A%7B%22url%22%3A%22ipfs%3A%2F%2FQmcxZHpyJa8T4i63xqjPYrZ6tKrt55tZJpbXcjSDKuKaf9%2Fethereum%2F512.png%22%2C%22width%22%3A512%2C%22height%22%3A512%2C%22format%22%3A%22png%22%7D%7D&clientId=9628d03675a74a399d2569a4b00bca20&theme=light&primaryColor=purple"
+          width="600px"
+          height="600px"
+          // style="max-width:100%;"
+          frameBorder="0"></iframe>
+
+        {/* <Button
           mt={4}
           colorScheme="blue"
           variant="outline"
@@ -102,7 +110,7 @@ export default function Home() {
           <Text py={4} fontSize="14px" color="#45a2f8">
             <LinkComponent href={txLink ? txLink : ''}>{txHash}</LinkComponent>
           </Text>
-        )}
+        )} */}
       </main>
     </>
   )
